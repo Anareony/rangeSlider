d2fca3b7d3fb99136b7c57a39ae0fd7a74008431
Anareony <chudoyakov.roman@gmail.com>
2021-12-28 18:06:48 +0700

    added ts, css

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,32 @@
+const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+
 module.exports = {
+    entry: "./src/index.ts",
     output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
         clean: true
     },
+
+    devtool: "source-map",
+    
+    resolve: {
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".jsx"]
+    },
+
     plugins: [
         new HTMLWebpackPlugin({
             template: "./src/index.html"
         })
-    ]
+    ],
+
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            { test: /\.js?$/, loader: "source-map-loader" },
+            { test: /\.css$/i, use: ["style-loader", "css-loader"]}
+        ]
+    }
 }
